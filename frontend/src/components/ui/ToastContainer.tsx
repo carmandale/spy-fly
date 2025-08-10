be68aaa8d3fdb3bd0fedce673f14ebd396c30c24
Dale Carman <dale.carman@gmail.com>
2025-08-10 07:24:54 -0500

    fix: export ToastData as interface instead of type #28

--- frontend/src/components/ui/ToastContainer.tsx
+++ frontend/src/components/ui/ToastContainer.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Toast } from './Toast'
-import type { ToastProps } from './Toast'
+import { Toast, type ToastProps } from './Toast'
 
-export type ToastData = Omit<ToastProps, 'onClose'>
+export interface ToastData extends Omit<ToastProps, 'onClose'> {}
 
 interface ToastContainerProps {
 	toasts: ToastData[]
