2b7837f45fa216d828bdbd396f00e2df4ff5ec64
Dale Carman <dale.carman@gmail.com>
2025-08-10 06:45:53 -0500

    debug: temporarily disable React Router to diagnose blank page issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
-import ExecutionPage from './pages/ExecutionPage'
+// import ExecutionPage from './pages/ExecutionPage'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/execute/:recommendationId" element={<ExecutionPage />} />
-      </Routes>
-    </Router>
-  )
+  console.log('App component rendering')
+  return <Dashboard />
+  // return (
+  //   <Router>
+  //     <Routes>
+  //       <Route path="/" element={<Dashboard />} />
+  //       <Route path="/execute/:recommendationId" element={<ExecutionPage />} />
+  //     </Routes>
+  //   </Router>
+  // )
 }
 
 export default App
